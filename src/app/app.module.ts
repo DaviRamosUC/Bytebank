13c63b31d155f29817fc5c22a4f8f2cb00635d88
Davi Ramos Lima <davi.lima@ucsal.edu.br>
2021-08-31 23:33:08 -0300

    Criando e estilizando tabela de extrato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+import { DEFAULT_CURRENCY_CODE } from '@angular/core';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -16,7 +21,7 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }, { provide: DEFAULT_CURRENCY_CODE, useValue: "BR" }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
